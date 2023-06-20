2cc720d9ac9e9f7860c646ad22a5ed28501de0a0
chirag4851 <chiragchavda1243@gmail.com>
2023-06-21 02:56:42 +0530

    debugging hamburger

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -11,23 +11,27 @@ const Notesdisplay = dynamic(() => import("@/components/Notes"));
 
 export default function Notes() {
   const [isOpen, setIsOpen] = useState(false);
-  const [customClass, setcustomClass] = useState({ sidebar: "hidden" });
-  const [renderFile, setrenderFile] = useState("Notesdisplay");
+  const [customClass, setCustomClass] = useState({ sidebar: "hidden" });
+  const [renderFile, setRenderFile] = useState("Notesdisplay");
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      // This code will run only on the client-side
+    if (typeof window !== undefined){
+    if (isOpen) {
       const path = window.location.pathname;
       if (path === "/notes") {
-        setcustomClass({
+        setCustomClass({
           sidebar: "flex-1 bg-slate-900 max-w-xs h-full pb-72",
         });
-      }
+      } 
     }
-  }, []);
+    else {
+      setCustomClass({ sidebar: "hidden" });
+    }}
+  }, [isOpen]);
 
   const clickHandler = () => {
     setIsOpen((prev) => !prev);
+    console.log('clickHandler - isOpen:', isOpen);
   };
 
   const renderComponent = () => {
@@ -44,7 +48,7 @@ export default function Notes() {
     <div>
       <Navbar />
       <Image
-        className="absolute top-9 left-9 animate-pop-out"
+        className="absolute top-9 left-9 animate-pop-out cursor-pointer"
         onClick={clickHandler}
         src="/hamburger.svg"
         width={25}
@@ -53,21 +57,21 @@ export default function Notes() {
       />
       <div className="flex">
         {/* sidebar */}
-        <div className={`${customClass.sidebar}`}>
+        <div className={customClass.sidebar}>
           <h1
             onClick={() => {
-              setrenderFile("Notesdisplay");
+              setRenderFile("Notesdisplay");
             }}
-            className="text-lg font-bold mt-5 mb-2 p-2 mx-5 w-[68px] text-yellow-400 border border-yellow-500"
+            className="text-lg font-bold mt-5 mb-2 p-2 mx-5 w-[68px] text-yellow-400 border border-yellow-500 cursor-pointer"
           >
             Notes
           </h1>
           <q className="mx-6 text-yellow-600 mt-1 mb-5 text-sm">Your notes</q>
           <h1
             onClick={() => {
-              setrenderFile("Pronote");
+              setRenderFile("Pronote");
             }}
-            className="text-lg font-bold mt-5 mb-2 p-2 mx-5 w-24 text-yellow-400 border border-yellow-500"
+            className="text-lg font-bold mt-5 mb-2 p-2 mx-5 w-24 text-yellow-400 border border-yellow-500 cursor-pointer"
           >
             Pro Note
           </h1>
@@ -76,9 +80,9 @@ export default function Notes() {
           </q>
           <h1
             onClick={() => {
-              setrenderFile("Zen");
+              setRenderFile("Zen");
             }}
-            className="text-lg font-bold mt-8 mb-2 p-2 mx-5 w-36 text-yellow-400 border border-yellow-500"
+            className="text-lg font-bold mt-8 mb-2 p-2 mx-5 w-36 text-yellow-400 border border-yellow-500 cursor-pointer"
           >
             Zen Typeaway
           </h1>
